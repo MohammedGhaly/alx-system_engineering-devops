bce7121184296b1b8bc4943575be0054b226a18e
MohammedGhaly <medooghaly7@gmail.com>
2023-10-10 08:15:30 -0700

    task 0 fixed

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
 """a module tha includes a function that queries the Reddit API"""
 import requests
-import sys
 
 
 def number_of_subscribers(subreddit):
-    res = requests.get(f"https://www.reddit.com/r/{sys.argv[1]}/about.json")
+    if subreddit is None or type(subreddit) is not str:
+        return 0
+    res = requests.get(f"https://www.reddit.com/r/{subreddit}/about.json",
+                       headers={'User-Agent': '0x16-api_advanced:project:\
+v1.0.0'})
     dic = res.json()
-    subs = dic.get('data').get('subscribers')
+    if dic is None:
+        return 0
+    subs = dic.get('data')
+    subs = subs.get('subscribers')
     return 0 if (subs is None) else subs
