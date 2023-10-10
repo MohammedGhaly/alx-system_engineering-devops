fffe3ce9ec702432127b0d6fb0431e9f4b964fe0
MohammedGhaly <medooghaly7@gmail.com>
2023-10-10 08:23:52 -0700

    task 0 solved

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
-"""a module tha includes a function that queries the Reddit API"""
+"""
+Contains the number_of_subscribers function
+"""
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """returns the number of subscribers for a given subreddit"""
+    """returns the number of subscribers for a subreddit"""
     if subreddit is None or type(subreddit) is not str:
         return 0
-    res = requests.get(f"https://www.reddit.com/r/{subreddit}/about.json",
-                       headers={'User-Agent': '0x16-api_advanced:project:\
-v1.0.0'})
-    dic = res.json()
-    if dic is None:
-        return 0
-    subs = dic.get('data')
-    subs = subs.get('subscribers')
-    return 0 if (subs is None) else subs
+    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                     headers={'User-Agent': '0x16-api_advanced:project:\
+v1.0.0'}).json()
+    sub = r.get("data", {}).get("subscribers", 0)
+    return sub
