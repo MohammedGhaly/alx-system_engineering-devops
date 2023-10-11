39b2e32e49f2da3aa44a63bb2c19bb57eb0be573
MohammedGhaly <medooghaly7@gmail.com>
2023-10-11 05:50:33 -0700

    fixed task 0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # replacing 'phpp' typo with 'php' in the file /var/www/html/wp-settings
 
-exec {
+exec { 'fix_wordpress':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => '/usr/local/bin/:/bin/'
 }
